ab84bf4594a91495585d2fad7e22c7cd0759b839
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-10-09 17:30:01 +0900

    first commit

---
+++ instagram-login/instagram_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_loginApp.swift
+//  instagram-login
+//
+//  Created by 水原　樹 on 2024/10/09.
+//
+
+import SwiftUI
+
+@main
+struct instagram_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
